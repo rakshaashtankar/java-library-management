475c389e27845cfe18722f371480c7a05cc06c91
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-02-10 19:53:11 +0530

    Add LibraryApp with menu loop and switch structure

--- src/com/librarymanagement/app/LibraryApp.java
+++ src/com/librarymanagement/app/LibraryApp.java
@@ -1,5 +1,49 @@
 package com.librarymanagement.app;
 
+import java.util.Scanner;
+
+import com.librarymanagement.service.LibraryService;
+
 public class LibraryApp {
+	public static void main(String[] args) {
+		System.out.println("Library Management System Started...");
+		Scanner sc = new Scanner(System.in);
+		LibraryService libraryService = new LibraryService();
+		boolean exit = false;
+		while(!exit) {
+			System.out.println("=== Library Management System Menu ===");	
+			System.out.println("1. Add Book");
+			System.out.println("2. View Books");
+			System.out.println("3. Search Book");
+			System.out.println("4. Update Book");
+			System.out.println("5. Issue Book");
+			System.out.println("6. Return Book");
+			System.out.println("7. Exit");
+			
+			System.out.println("Enter your choice: ");
+			int choice = sc.nextInt();
+			sc.nextLine();
+			
+			switch(choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			default:
+				System.out.println("Invalid choice");
+			}
+		}
+		sc.close();
+	}
 
 }
