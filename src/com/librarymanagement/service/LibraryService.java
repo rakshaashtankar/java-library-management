810a6afa2e7e79eaa5c549e9fde27f0a1cd4ff6d
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-02-10 19:29:56 +0530

    Add LibraryService sekeleton with fields and method declarations

--- src/com/librarymanagement/service/LibraryService.java
+++ src/com/librarymanagement/service/LibraryService.java
@@ -1,5 +1,54 @@
 package com.librarymanagement.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.librarymanagement.model.Book;
+
 public class LibraryService {
+	
+	private List<Book> books = new ArrayList<>();
+	private int counter = 1;
+	
+	public boolean addBook(Book book) {
+		return false;
+	}
+	
+	public List<Book> viewAllBooks() {
+		return new ArrayList<>(books);
+	}
+	
+	public List<Book> searchBookByTitle(String title) {
+		return null;
+	}
+	
+	public List<Book> searchBookByAuthor(String author) {
+		return null;
+	}
+	
+	public boolean updateBook(Book book) {
+		return false;
+	}
+	
+	public boolean issueBook(int id) {
+		return false;
+	}
+	
+	public boolean returnBook(int id) {
+		return false;
+	}
+
+	
+
+	public boolean saveToFile(Book book) {
+		return false;
+	}
+	
+	public boolean loadFromFile() {
+		return false;
+	}
+
+	
+	
 
 }
